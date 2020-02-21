b3a3edca4e6e23b84704ab32b7cb8b9b82778cb3
Kenneth Spatta <kenyounot@gmail.com>
2020-02-21 17:45:59 -0500

    Create create, authenticate, and profile actions for user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,50 @@
 class Api::V1::UsersController < ApplicationController
+  skip_before_action :authenticate_request, :except => [:profile]
 
   def create
-    @user = User.new(user_params)
+    user = User.new(user_params)
 
-    if @user.save
+    if user.save
+      command = AuthenticateUser.call(user_params[:email], user_params[:password])
       render json: {
-        user: @user
+        user: {
+          name: user.name,
+          email: user.email,
+          id: user.id,
+        },
+        jwt: command.result,
       }
     end
+  end
+
+  def authenticate
+    command = AuthenticateUser.call(user_params[:email], user_params[:password])
+
+    if command.success?
+      user = User.find_by(email: user_params[:email])
+      render json: {
+        user: {
+          name: user.name,
+          email: user.email,
+          id: user.id,
+        },
+        jwt: command.result,
+      }
+    else
+      render json: { error: command.errors }, status: :unauthorized
+    end
+  end
+
+  def profile
+    @user = current_user
+
+    render json: {
+      user: {
+        id: @user.id,
+        name: @user.name,
+        email: @user.email,
+      },
+    }
   end
 
   private
