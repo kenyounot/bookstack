7f578fcdf8a347932fedcb65251302ac78dae362
Kenneth Spatta <kenyounot@gmail.com>
2020-02-21 14:48:39 -0500

    Add create action to users controller, also migrate user password name change to password_digest

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,20 @@
 class Api::V1::UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: {
+        user: @user
+      }
+    end
+  end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
