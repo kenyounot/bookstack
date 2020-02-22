96f08bedfaf937d55b8b47ba3f60ccbbfc3ffd09
Kenneth Spatta <kenyounot@gmail.com>
2020-02-22 12:16:48 -0500

    Create book model with associations

---
+++ db/migrate/20200222171602_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :summary
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :author, null: false, foreign_key: true
+      t.belongs_to :genre, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
