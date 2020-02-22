9e694bce3c3a996d9ddca1546c1988e5efc14f1f
Kenneth Spatta <kenyounot@gmail.com>
2020-02-22 13:23:03 -0500

    Create books controller with crud action methods defined

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::BooksController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'books/index'
+      get 'books/create'
+      get 'books/update'
+      get 'books/delete'
+    end
+  end
+  get 'book/index'
+  get 'book/create'
+  get 'book/update'
+  get 'book/delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   namespace :api do
