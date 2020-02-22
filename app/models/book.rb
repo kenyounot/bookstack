d51e5b9b275ce90b23e27b92ed98dc06ee05968c
Kenneth Spatta <kenyounot@gmail.com>
2020-02-22 18:54:50 -0500

    Add validations for book, all attributes must be present, summary will now have a max length

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_many :notes, :dependent => :destroy
+
+  validates :title, :summary, :author, :genre, :img_url, presence: true
+  validates :summary, length: { maximum: 140 }
 end
