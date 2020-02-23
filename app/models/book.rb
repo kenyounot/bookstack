2ead169031fdb823770d17e39650e9d305bc4b5c
Kenneth Spatta <kenyounot@gmail.com>
2020-02-22 19:15:23 -0500

    Add validation for book, not allowing the same book title to be added twice

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :notes, :dependent => :destroy
 
+  validates_uniqueness_of :title, scope: :user_id
   validates :title, :summary, :author, :genre, :img_url, presence: true
   validates :summary, length: { maximum: 140 }
 end
