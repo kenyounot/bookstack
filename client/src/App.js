434b8dc496aaf894bc82946133e694ed878bd06a
Kenneth Spatta <kenyounot@gmail.com>
2020-02-23 17:25:47 -0500

    Set app to grab all books when the app component mounts if a user is logged in

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import NavBar from './components/navBar/NavBar';
 import './styles/App.css';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { fetchBooks } from './actions/fetchBooks';
 import Login from './containers/Login';
 import Signup from './containers/Signup';
 import Home from './containers/Home';
 
-function App() {
-	return (
-		<div className='app'>
-			<NavBar />
-			<Switch>
-				<Route exact path='/login' component={Login} />
-				<Route exact path='/signup' component={Signup} />
-				<Route exact path='/home' component={Home} />
-			</Switch>
-		</div>
-	);
+class App extends Component {
+	componentDidMount() {
+		if (localStorage.getItem('token')) {
+			// this.props.fetchProfile();
+			this.props.fetchBooks();
+		}
+	}
+
+	render() {
+		return (
+			<div className='app'>
+				<NavBar />
+				<Switch>
+					<Route exact path='/login' component={Login} />
+					<Route exact path='/signup' component={Signup} />
+					<Route exact path='/home' component={Home} />
+					<Route exact path='/collection' component={Collection} />
+				</Switch>
+			</div>
+		);
+	}
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+	return {
+		fetchBooks: () => dispatch(fetchBooks())
+	};
+};
+
+export default connect(null, mapDispatchToProps)(App);
