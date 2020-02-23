438ea5bb2f13da078be79168dbe22bec8b30dce1
Kenneth Spatta <kenyounot@gmail.com>
2020-02-23 13:49:54 -0500

    Create handle on change function for the search bar

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react';
 import Search from '../components/Search';
 
 export class Home extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			books: [],
+			searchTerm: ''
+		};
+	}
+
+	handleOnChange = evt => {
+		this.setState({
+			...this.state,
+			[evt.target.name]: evt.target.value
+		});
+	};
+
 	render() {
 		return (
 			<div>
