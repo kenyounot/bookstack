5e95b458720690ab636bcd6b80f1110367d9a6f8
Kenneth Spatta <kenyounot@gmail.com>
2020-02-22 23:05:44 -0500

    Install redux and redux thunk to handle state

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './styles/index.css';
 import App from './App';
+import { configureStore } from './store';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+const store = configureStore();
+
 ReactDOM.render(
-	<Router>
-		<App />
-	</Router>,
+	<Provider store={store}>
+		<Router>
+			<App />
+		</Router>
+	</Provider>,
 	document.getElementById('root')
 );
