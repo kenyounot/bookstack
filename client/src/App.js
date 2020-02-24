55aa65845d6a5607fcf120933ffa8ec449dbf6b9
Kenneth Spatta <kenyounot@gmail.com>
2020-02-24 02:59:07 -0500

    Add logout action to log user out and clear token

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,8 @@ class App extends Component {
 		}
 	}
 
+	handleLogout = () => {};
+
 	render() {
 		return (
 			<div className='app'>
--- client/src/components/navBar/NavBar.js
+++ client/src/components/navBar/NavBar.js
@@ -2,7 +2,51 @@ import React from 'react';
 import './NavBar.css';
 import { Link } from 'react-router-dom';
 
-export default function NavBar() {
+export default function NavBar(props) {
+	renderNavItems = () => {
+		if (localStorage.getItem('token')) {
+			return (
+				<div className='nav-wrapper'>
+					<div className='left-side'>
+						<div className='brand'>
+							<div>BookStack</div>
+						</div>
+					</div>
+
+					<div className='right-side'>
+						<div className='nav-link-wrapper'>
+							<Link to='/search'>Add Book</Link>
+						</div>
+						<div className='nav-link-wrapper'>
+							<Link onClick={props.handleLogout} to='/login'>
+								logout
+							</Link>
+						</div>
+					</div>
+				</div>
+			);
+		} else {
+			return (
+				<div className='nav-wrapper'>
+					<div className='left-side'>
+						<div className='brand'>
+							<div>BookStack</div>
+						</div>
+					</div>
+
+					<div className='right-side'>
+						<div className='nav-link-wrapper'>
+							<Link to='/login'>Login</Link>
+						</div>
+						<div className='nav-link-wrapper'>
+							<Link to='/signup'>Signup</Link>
+						</div>
+					</div>
+				</div>
+			);
+		}
+	};
+
 	return (
 		<div className='nav-wrapper'>
 			<div className='left-side'>
