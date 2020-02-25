ab86c126f85ce4f9567fa92826575e50294fce95
Kenneth Spatta <kenyounot@gmail.com>
2020-02-25 05:13:46 -0500

    Reconfigure Cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,14 +1,16 @@
-require ::File.expand_path("../config/environment", __FILE__)
-run Rails.application
+# Be sure to restart your server when you modify this file.
 
-require "rack/cors"
-use Rack::Cors do
+# Avoid CORS issues when API is called from the frontend app.
+# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
-  # allow all origins in development
+# Read more: https://github.com/cyu/rack-cors
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins "http://localhost:3001"
+
     resource "*",
-             :headers => :any,
-             :methods => [:get, :post, :delete, :put, :options]
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
