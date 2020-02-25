d4f231b4a76a5923398246d2e7c148014f601042
Kenneth Spatta <kenyounot@gmail.com>
2020-02-25 02:30:14 -0500

    Change my foreman tasks back to they originally were

--- lib/tasks/start.rake
+++ lib/tasks/start.rake
@@ -1,5 +1,3 @@
-namespace :start do
-  task :production do
-    exec "NPM_CONFIG_PRODUCTION=true npm run postinstall && foreman start"
-  end
+task :start do
+  exec "foreman start -p 3001"
 end
---
+++ public/precache-manifest.85f76366520e291f276052a64224ceab.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5162b0776ab2cf453e011a6204df78b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d90da2c79950abd97ed",
+    "url": "/static/css/main.83834f78.chunk.css"
+  },
+  {
+    "revision": "2296805607d7a17a8800",
+    "url": "/static/js/2.d20906dc.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.d20906dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d90da2c79950abd97ed",
+    "url": "/static/js/main.5bf021c9.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "a169b3a2b3f807bb0bdbaccfad60a91e",
+    "url": "/static/media/404.a169b3a2.jpg"
+  },
+  {
+    "revision": "4ed46ff577a3bfb5b5c1fa727f006f3e",
+    "url": "/static/media/hero-book.4ed46ff5.jpeg"
+  },
+  {
+    "revision": "8a45fecf9766ea8706f5cb3b4bd6b99b",
+    "url": "/static/media/photo-1532012197267-da84d127e765.8a45fecf.jpeg"
+  }
+]);
