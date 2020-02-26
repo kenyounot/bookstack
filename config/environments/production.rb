7fdf8f00ba037c43c5cf7d4ca1cbe26d1ddb1ccb
Kenneth Spatta <kenyounot@gmail.com>
2020-02-25 22:08:52 -0500

    Add timezone to production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,8 @@ Rails.application.configure do
 
   config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
+  config.time_zone = "Eastern Time (US & Canada)"
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
